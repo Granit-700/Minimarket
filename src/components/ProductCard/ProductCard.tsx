8a5311fe74aed46cb84f8a6b1dacce91fdc8545b
Granit_700 <114147712+Granit-700@users.noreply.github.com>
2026-07-04 22:51:51 +0600

    fix: use imported assets instead of relative paths

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 import type { ProductCardProps } from "../../types";
 import "./ProductCard.css";
 import { useAppStore } from "../../api/appStore";
+import editIcon from "../../assets/icons/edit.svg";
+import deleteIcon from "../../assets/icons/delete.svg";
+import defaultImage from "../../assets/icons/image-default.svg";
 
 const ProductCard = (props: ProductCardProps) => {
   const {
@@ -15,18 +18,14 @@ const ProductCard = (props: ProductCardProps) => {
       <div className="card_header">
         <button className="card_btn">
           <Link to={`/products/${id}/edit`}>
-            <img src="src/assets/icons/edit.svg" alt="edit" />
+            <img src={editIcon} alt="edit" />
           </Link>
         </button>
         <button className="card_btn" onClick={() => deleteProduct(id)}>
-          <img src="src/assets/icons/delete.svg" alt="delete" />
+          <img src={deleteIcon} alt="delete" />
         </button>
       </div>
-      <img
-        src={imageURL || "src/assets/icons/image-default.svg"}
-        alt={title}
-        className="card_image"
-      />
+      <img src={imageURL || defaultImage} alt={title} className="card_image" />
       <div className="card_text">
         <h3>{title}</h3>
         <p>{description}</p>
